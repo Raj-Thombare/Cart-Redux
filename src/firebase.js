0d9097b7293f6cd74ee246cdef1d3ebaccf1d79c
Rakaa <therajthombare@gmail.com>
2022-11-09 00:17:27 +0530

    connected firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCD4gdtnkQh097d1ZnHVa08dYDOU_z0zIM",
+  authDomain: "cart-redux-f2219.firebaseapp.com",
+  projectId: "cart-redux-f2219",
+  storageBucket: "cart-redux-f2219.appspot.com",
+  messagingSenderId: "310471717260",
+  appId: "1:310471717260:web:c9a042b86e5a8cfb8ba6a9"
+};
+
+const app = initializeApp(firebaseConfig);
